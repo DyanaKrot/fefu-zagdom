4d92df812bdbacee50ba6ac5bc92da71234746ba
DyanaKrot <81305949+DyanaKrot@users.noreply.github.com>
2022-02-02 16:12:48 +1000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+   
     public function register()
     {
-        //
+    
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        $loader = new \Twig\Loader\FilesystemLoader();
+        $loader->addPath(base_path() . '/resources/components', 'components');
+        $loader->addPath(base_path() . '/resources/layouts', 'layouts');
+        \Twig::getLoader()->addLoader($loader);
     }
 }
